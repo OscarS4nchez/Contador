3fc2b155ada9e447521ff6e6f8236f4837a03a69
oscars4nchez <17470008@campeche.tecnm.mx>
2020-10-23 19:50:47 -0500

    Version inicial del proyecto. Sin funcionalidades implementadas.

--- app/src/main/java/com/oscaruc/contador/MainActivity.java
+++ app/src/main/java/com/oscaruc/contador/MainActivity.java
@@ -3,6 +3,9 @@ package com.oscaruc.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final int[] mConteo = {0};
+        final TextView mConteoTextView = findViewById(R.id.conteo_text_view);
+        final Button mContarButton = findViewById(R.id.contar_button);
+        final Button mReiniciarButton = findViewById(R.id.reiniciar_button);
+
+        mContarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mConteo[0]++;
+                mConteoTextView.setText(Integer.toString(mConteo[0]));
+            }
+        });
+
+        mReiniciarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 mConteo[0] = 0;
+                mConteoTextView.setText(Integer.toString(mConteo[0]));
+            }
+        });
+
+
+
+
+
+
     }
 }
