83f2676ab393b6857ec763104f2571c12ab798e1
oscars4nchez <17470008@campeche.tecnm.mx>
2020-10-23 21:20:23 -0500

    Corrge la perdida del conteo al acambiar la configuracion del dispositivo de vertial a horizontal y viceversa.

--- app/src/main/java/com/oscaruc/contador/MainActivity.java
+++ app/src/main/java/com/oscaruc/contador/MainActivity.java
@@ -1,45 +1,64 @@
 package com.oscaruc.contador;
 
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private final String CONTEO = "com.oscaruc.contador.conteo";
+    private TextView mConteoTextView;
+    private Button mContarButton;
+    private Button mReiniciarButton;
+    private int mConteo;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(CONTEO, mConteo);
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConteo = 0;
+        mConteoTextView = findViewById(R.id.conteo_text_view);
+        mContarButton = findViewById(R.id.contar_button);
+        mReiniciarButton = findViewById(R.id.reiniciar_button);
 
-
-        final int[] mConteo = {0};
-        final TextView mConteoTextView = findViewById(R.id.conteo_text_view);
-        final Button mContarButton = findViewById(R.id.contar_button);
-        final Button mReiniciarButton = findViewById(R.id.reiniciar_button);
-
+        if (savedInstanceState != null) {
+            mConteo = savedInstanceState.getInt(CONTEO);
+        }
+        else {
+            mConteo = 0;
+        }
+        mConteoTextView.setText(String.format(Locale.getDefault(), "%d", mConteo));
         mContarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mConteo[0]++;
-                mConteoTextView.setText(Integer.toString(mConteo[0]));
+                mConteo++;
+                mConteoTextView.setText(
+                        String.format(Locale.getDefault(), "%d", mConteo));
             }
         });
-
         mReiniciarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 mConteo[0] = 0;
-                mConteoTextView.setText(Integer.toString(mConteo[0]));
+                mConteo = 0;
+                mConteoTextView.setText(
+                        String.format(Locale.getDefault(), "%d", mConteo));
             }
+
         });
-
-
-
-
-
-
     }
 }
